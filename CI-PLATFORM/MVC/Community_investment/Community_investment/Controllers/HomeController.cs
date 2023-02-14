e5b32c9c7a2d3b69c756bd478cdfb223bb465138
KomalKamani1552 <97843807+KomalKamani1552@users.noreply.github.com>
2023-02-14 13:31:27 +0530

    Files upload

---
+++ CI-PLATFORM/MVC/Community_investment/Community_investment/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Community_investment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Community_investment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Forgotpassword()
+        {
+            return View();
+        }
+        public IActionResult Resetpassword()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
